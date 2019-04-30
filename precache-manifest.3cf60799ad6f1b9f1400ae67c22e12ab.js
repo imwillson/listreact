16cf2a3e0695c02e5096f44e90b8b2ece6dcabd8
Willson Li <willson@willsons-mbp.home>
2019-04-30 08:41:43 -0400

    Updates

---
+++ precache-manifest.3cf60799ad6f1b9f1400ae67c22e12ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2529719a5041e236f73e",
+    "url": "/listreact/static/js/runtime~main.ae1abafb.js"
+  },
+  {
+    "revision": "26a3cfe65ec3d43991e2",
+    "url": "/listreact/static/js/main.e025a51c.chunk.js"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/listreact/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "26a3cfe65ec3d43991e2",
+    "url": "/listreact/static/css/main.9cda8736.chunk.css"
+  },
+  {
+    "revision": "b189570a1e2ccd57bf72adb8021283d0",
+    "url": "/listreact/index.html"
+  }
+];
